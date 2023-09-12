571aa0795fdd09343e606e0ab3cbc31967e49586
José Rosendo <jose.l.rosendo.s@gmail.com>
2023-09-12 11:47:50 -0300

    Create footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Cou from "./components/Cou";
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
       <Cou />
+      <footer className="custom-footer">
+        <span>Versión: 0.1.1</span>
+        &nbsp;
+        <span>|</span>
+        &nbsp;
+        <span><a href="https://www.linkedin.com/in/jrosendo/" target="_blank">Creador</a></span>
+      </footer>
     </div>
   );
 };
