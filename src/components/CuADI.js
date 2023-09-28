6261edfa69753717a7e230e98f61e60d69d006da
José Rosendo <jose.l.rosendo.s@gmail.com>
2023-09-28 15:54:16 -0300

    Retrieve values from COU

--- src/components/CuADI.js
+++ src/components/CuADI.js
@@ -16,12 +16,107 @@ const CuADI = ({ appValues }) => {
   const branchesIndexes = _.range(1, appValues.branches.length + 1);
   /******************************************************************************/
   const emptyCuADIByInstitutionalSectors = {
-    // TODO: implement defining skeleton for empty table data
+    // TODO: customize this object when copying file content to another
+    sbsx: {
+      resource: {
+        rm: null,
+        total: null,
+      },
+    },
+    eeb: {
+      resource: {
+        society: null,
+        gov: null,
+        st: null,
+        total: null,
+      },
+    },
+    ra: {
+      resource: {
+        homes: null,
+        st: null,
+        total: null,
+      },
+    },
+    tax: {
+      resource: {
+        gov: null,
+        st: null,
+        total: null,
+      },
+    },
+    rp: {
+      resource: {
+        homes: null,
+        st: null,
+        rm: null,
+        total: null,
+      },
+      usage: {
+        society: null,
+        gov: null,
+        st: null,
+        rm: null,
+        total: null,
+      },
+    },
+    cs: {
+      resource: {
+        gov: null,
+        st: null,
+        total: null,
+      },
+      usage: {
+        homes: null,
+        st: null,
+        total: null,
+      },
+    },
+    ps: {
+      resource: {
+        homes: null,
+        st: null,
+        total: null,
+      },
+      usage: {
+        gov: null,
+        st: null,
+        total: null,
+      },
+    },
+    otc: {
+      resource: {
+        gov: null,
+        st: null,
+        total: null,
+      },
+      usage: {
+        society: null,
+        st: null,
+        rm: null,
+        total: null,
+      },
+    },
+    idb: {
+      usage: {
+        society: null,
+        gov: null,
+        homes: null,
+        st: null,
+        total: null,
+      },
+    },
+    scx: {
+      usage: {
+        rm: null,
+        total: null,
+      },
+    },
   };
   const storedCuCuADIByInstitutionalSectors = getItem(
     "cuADIByInstitutionalSectors"
   );
-  const [cuADIByInstitutionalSectors, setcuADIByInstitutionalSectors] =
+  const [CuADIByInstitutionalSectors, setcuADIByInstitutionalSectors] =
     useState(
       _.cloneDeep(
         storedCuCuADIByInstitutionalSectors || emptyCuADIByInstitutionalSectors
@@ -32,14 +127,14 @@ const CuADI = ({ appValues }) => {
     setItem("cuADIByInstitutionalSectors", content);
   };
   const handlecuADIByInstitutionalSectorsValueChange = (value, path) => {
-    _.set(cuADIByInstitutionalSectors, path, value);
-    savecuADIByInstitutionalSectorsValues(cuADIByInstitutionalSectors);
+    _.set(CuADIByInstitutionalSectors, path, value);
+    savecuADIByInstitutionalSectorsValues(CuADIByInstitutionalSectors);
   };
   const computeByInstitutionalSectors = () => {
     console.log(
       "Calculando valores de Cuenta de Asignación y Distribución de Ingresos por Sectores Institucionales"
     );
-    // TODO: build equations generators
+    // TODO: customize this segment when copying file content to another (build equations generators)
     let hasComputed = false;
     let maxAmountOfIterations = 100;
     do {
@@ -51,7 +146,20 @@ const CuADI = ({ appValues }) => {
         let side;
         let cols;
         const equationsGenerators = [];
-        // TODO: build assignation of equations generators according to current row
+        // TODO: customize this segment when copying file content to another (build assignation of equations generators according to current row)
+
+        if (row === "sbsx") {
+        } else if (row === "eeb") {
+        } else if (row === "ra") {
+        } else if (row === "tax") {
+        } else if (row === "rp") {
+        } else if (row === "cs") {
+        } else if (row === "ps") {
+        } else if (row === "otc") {
+        } else if (row === "idb") {
+        } else if (row === "scx") {
+        }
+
         let iCols = 0;
         while (iCols < cols.length && !hasComputed) {
           const col = cols[iCols];
@@ -68,14 +176,14 @@ const CuADI = ({ appValues }) => {
             let iEquations = 0;
             while (iEquations < equations.length && !hasComputed) {
               const equation = equations[iEquations];
-              if (isEquationSolvable(equation, cuADIByInstitutionalSectors)) {
+              if (isEquationSolvable(equation, CuADIByInstitutionalSectors)) {
                 console.log(`Solving ${equation.join(" ")}`);
                 const { leftSide, rightSide } = buildEquationSides(
                   equation,
-                  cuADIByInstitutionalSectors
+                  CuADIByInstitutionalSectors
                 );
                 hasComputed = shouldCompute(
-                  cuADIByInstitutionalSectors,
+                  CuADIByInstitutionalSectors,
                   solveEquation(leftSide, rightSide),
                   `${row}.${side}.${col}`
                 );
@@ -97,18 +205,72 @@ const CuADI = ({ appValues }) => {
         "Se ha alcanzado el máximo número de iteraciones para calcular la Cuenta de Asignación y Distribución de Ingresos por Sectores Institucionales"
       );
     }
-    savecuADIByInstitutionalSectorsValues(cuADIByInstitutionalSectors);
+    savecuADIByInstitutionalSectorsValues(CuADIByInstitutionalSectors);
   };
   const emptyByInstitutionalSectors = () => {
     savecuADIByInstitutionalSectorsValues(emptyCuADIByInstitutionalSectors);
   };
   const retrieveFromCouForByInstitutionalSectors = () => {
-    // TODO: implement retriveing and computing values from COU
+    // TODO: customize this segment when copying file content to another (implement retriveing and computing values from COU)
 
-    savecuADIByInstitutionalSectorsValues(cuADIByInstitutionalSectors);
+    // Saldo de Balanza Comercial
+    if (
+      hasContent(appValues.cou.imports.total) &&
+      hasContent(appValues.cou.totalUses.finalUse.exports)
+    ) {
+      const val = -(
+        _.toNumber(appValues.cou.totalUses.finalUse.exports) -
+        _.toNumber(appValues.cou.imports.total)
+      );
+      CuADIByInstitutionalSectors.sbsx.resource.rm = val;
+      CuADIByInstitutionalSectors.sbsx.resource.total = val;
+      CuADIByInstitutionalSectors.sbsx.resource.rm = val;
+      CuADIByInstitutionalSectors.sbsx.resource.total = val;
+    }
+
+    // EEB
+    if (
+      _.every(
+        branchesIndexes,
+        (idx) =>
+          hasContent(appValues.cou.een.intermediateUse[`branch${idx}`]) &&
+          hasContent(appValues.cou.ckf.intermediateUse[`branch${idx}`])
+      )
+    ) {
+      CuADIByInstitutionalSectors.eeb.resource.society = _.sum(
+        branchesIndexes.map(
+          (idx) =>
+            _.toNumber(appValues.cou.een.intermediateUse[`branch${idx}`]) +
+            _.toNumber(appValues.cou.ckf.intermediateUse[`branch${idx}`])
+        )
+      );
+    }
+    CuADIByInstitutionalSectors.eeb.resource.gov =
+      _.toNumber(appValues.cou.een.intermediateUse.gov) +
+      _.toNumber(appValues.cou.ckf.intermediateUse.gov);
+    CuADIByInstitutionalSectors.eeb.resource.st =
+      _.toNumber(appValues.cou.een.intermediateUse.st) +
+      _.toNumber(appValues.cou.ckf.intermediateUse.st);
+    CuADIByInstitutionalSectors.eeb.resource.total =
+      _.toNumber(appValues.cou.een.intermediateUse.st) +
+      _.toNumber(appValues.cou.ckf.intermediateUse.st);
+
+    // RA
+    CuADIByInstitutionalSectors.ra.resource.total =
+      appValues.cou.ra.intermediateUse.st;
+
+    // Taxes
+    CuADIByInstitutionalSectors.tax.resource.gov =
+      appValues.cou.tax.intermediateUse.st;
+    CuADIByInstitutionalSectors.tax.resource.st =
+      appValues.cou.tax.intermediateUse.st;
+    CuADIByInstitutionalSectors.tax.resource.total =
+      appValues.cou.tax.intermediateUse.st;
+
+    savecuADIByInstitutionalSectorsValues(CuADIByInstitutionalSectors);
   };
   const cellGeneratorForByInstitutionalSectors = new TableInputGenerator(
-    cuADIByInstitutionalSectors,
+    CuADIByInstitutionalSectors,
     handlecuADIByInstitutionalSectorsValueChange
   );
   /******************************************************************************/
@@ -124,7 +286,7 @@ const CuADI = ({ appValues }) => {
       </Button>
       &nbsp;
       <CrudModals
-        currentItem={cuADIByInstitutionalSectors}
+        currentItem={CuADIByInstitutionalSectors}
         storageKey="cuADIByInstitutionalSectors"
         saveModalTitle="Guardar Cuenta de Asignación y Distribución de Ingresos por Sectores Institucionales"
         loadModalTitle="Cargar Cuenta de Asignación y Distribución de Ingresos por Sectores Institucionales"
@@ -173,20 +335,30 @@ const CuADI = ({ appValues }) => {
               <strong>Saldo de bienes y servicios con el exterior</strong>
             </td>
             {DisabledCells(`sbsx`, 7, 4)}
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "sbsx.resource.rm"
+            )}
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "sbsx.resource.total"
+            )}
           </tr>
           <tr>
             {DisabledCells(`eeb`, 0, 6)}
             <td>
               <strong>Excedente de Explotación Bruto</strong>
             </td>
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "eeb.resource.society"
+            )}
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "eeb.resource.gov"
+            )}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("eeb.resource.st")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "eeb.resource.total"
+            )}
           </tr>
           <tr>
             {DisabledCells(`ra`, 0, 6)}
@@ -194,10 +366,14 @@ const CuADI = ({ appValues }) => {
               <strong>Remuneración de Asalariados</strong>
             </td>
             {DisabledCells(`ra`, 7, 2)}
-            <td></td>
-            <td></td>
-            <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "ra.resource.homes"
+            )}
+            {cellGeneratorForByInstitutionalSectors.generate("ra.resource.st")}
+            {cellGeneratorForByInstitutionalSectors.generate("ra.resource.rm")}
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "ra.resource.total"
+            )}
           </tr>
           <tr>
             {DisabledCells(`tax`, 0, 6)}
@@ -205,91 +381,109 @@ const CuADI = ({ appValues }) => {
               <strong>Impuestos - Subsidios producción</strong>
             </td>
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "tax.resource.gov"
+            )}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("tax.resource.st")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "tax.resource.total"
+            )}
           </tr>
           <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "rp.resource.total"
+            )}
+            {cellGeneratorForByInstitutionalSectors.generate("rp.resource.rm")}
+            {cellGeneratorForByInstitutionalSectors.generate("rp.resource.st")}
             <DisabledCell />
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("rp.resource.gov")}
+            {cellGeneratorForByInstitutionalSectors.generate("rp.resource.soc")}
             <td>
               <strong>Rentas de la propiedad</strong>
             </td>
             {DisabledCells(`rp`, 7, 2)}
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("rp.usage.homes")}
+            {cellGeneratorForByInstitutionalSectors.generate("rp.usage.st")}
+            {cellGeneratorForByInstitutionalSectors.generate("rp.usage.rm")}
+            {cellGeneratorForByInstitutionalSectors.generate("rp.usage.total")}
           </tr>
           <tr>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("cs.usage.total")}
             <DisabledCell />
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("cs.usage.st")}
+            {cellGeneratorForByInstitutionalSectors.generate("cs.usage.homes")}
             {DisabledCells(`cs`, 4, 2)}
             <td>
               <strong>Contribuciones sociales</strong>
             </td>
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("cs.resource.gov")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("cs.resource.st")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "cs.resource.total"
+            )}
           </tr>
           <tr>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("ps.usage.total")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("ps.usage.st")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("ps.usage.gov")}
             <DisabledCell />
             <td>
               <strong>Prestaciones sociales</strong>
             </td>
             {DisabledCells(`ps`, 7, 2)}
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "ps.resource.homes"
+            )}
+            {cellGeneratorForByInstitutionalSectors.generate("ps.resource.st")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "ps.resource.total"
+            )}
           </tr>
           <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("otc.usage.total")}
+            {cellGeneratorForByInstitutionalSectors.generate("otc.usage.rm")}
+            {cellGeneratorForByInstitutionalSectors.generate("otc.usage.st")}
             {DisabledCells(`otc`, 3, 2)}
             <td></td>
             <td>
               <strong>Otras transferencias corrientes</strong>
             </td>
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "otc.resource.gov"
+            )}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("otc.resource.st")}
             <DisabledCell />
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "otc.resource.total"
+            )}
           </tr>
           <tr>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("idb.usage.total")}
             <DisabledCell />
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("idb.usage.st")}
+            {cellGeneratorForByInstitutionalSectors.generate("idb.usage.homes")}
+            {cellGeneratorForByInstitutionalSectors.generate("idb.usage.gov")}
+            {cellGeneratorForByInstitutionalSectors.generate(
+              "idb.usage.society"
+            )}
             <td>
               <strong>Ingreso Disponible Bruto</strong>
             </td>
             {DisabledCells(`idb`, 6, 6)}
           </tr>
           <tr>
-            <td></td>
-            <td></td>
+            {cellGeneratorForByInstitutionalSectors.generate("scx.usage.total")}
+            {cellGeneratorForByInstitutionalSectors.generate("scx.usage.rm")}
             {DisabledCells(`scx`, 2, 4)}
             <td>
               <strong>Saldo Corriente con el Exterior</strong>
